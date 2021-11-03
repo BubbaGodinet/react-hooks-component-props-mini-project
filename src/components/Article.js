762e0a655e64338a825ab3660f88ba3c49c3ca79
BubbaGodinet <godinetvn@gmail.com>
2021-11-03 17:09:15 -0600

    commit tree fb5faefd14f9fd95b197e278e34b0fb7f539681f

---
+++ src/components/Article.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Article({key, post: {title, date = "January 1, 1978", preview, minutes}}) {
+    function handleCoffeeEmoji({minutes}){
+        if(minutes < 30){
+            let number = Math.ceil(minutes / 5)
+            let coffee = '☕';
+            let coffeeText = <small>{coffee.repeat(number)}</small>
+            return coffeeText
+        } else {
+            let number = Math.ceil(minutes / 10)
+            let bentoBox = '🍱';
+            let bentoText = <small>{bentoBox.repeat(number)}</small>
+            return bentoText
+        }
+    }
+
+    return(
+        <article>
+            <h3>{title}</h3>
+            <small>{date}</small>
+            <small>{handleCoffeeEmoji({minutes})}{minutes} to read</small>
+            <p>{preview}</p>
+        </article>
+    );
+}
+
+export default Article
